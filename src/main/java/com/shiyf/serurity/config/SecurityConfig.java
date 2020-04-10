84fa51431f1e6d868cab25fb306bebc9e205b431
shiyongfeng <shiyongfeng@sinosoft.com.sn>
2020-04-10 08:01:09 +0800

    修改为使用内存用户名密码的形式登录，添加了两个用户user和admin，密码为1234，使用BCyrptPasswordEncoder

--- src/main/java/com/shiyf/serurity/config/SecurityConfig.java
+++ src/main/java/com/shiyf/serurity/config/SecurityConfig.java
@@ -1,12 +1,36 @@
 package com.shiyf.serurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("1234"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("1234"))
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
